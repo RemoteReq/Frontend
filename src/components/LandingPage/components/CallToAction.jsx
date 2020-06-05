346ffa5d7490ea730add006d1da33387d171adf9
ryanferrer <ryferr8122@gmail.com>
2020-06-05 15:50:44 -0500

    stashing

--- src/components/LandingPage/components/CallToAction.jsx
+++ src/components/LandingPage/components/CallToAction.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CallToAction = () => (
   <div className='call-to-action'>
     <h1>Don't just work, do something greater.</h1>
     <div className='call-to-action-btns'>
-        <button className='cta-find-jobs'>find jobs</button>
-        <button className='cta-find-talent'>find talent</button>
+        <button className='cta-find-jobs'><Link to="/signup">Find Jobs</Link></button>
+        <button className='cta-find-talent'><Link to="/findTalent">Find Talent</Link></button>
     </div>
   </div>
 );
--- src/components/LandingPage/components/LPHeader.jsx
+++ src/components/LandingPage/components/LPHeader.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const LPHeader = () => (
   <div className='landingPage-header'>
@@ -6,15 +7,15 @@ const LPHeader = () => (
     <div className='landingPage-heading-btns'>
       <div className='heading'>
         <h1>Find remote work with purpose.</h1>
-        { window.innerWidth > 767? (
+        { window.innerWidth > 767 ? (
           <h3>We connect highly-skilled, remote professionals with nonprofit and for-profit companies working for the greater good. Find a part-time gig, or full-time, today.</h3>
 
-        ) :
-          <h3>We connect highly-skilled, remote professionals with companies working for the greater good.</h3>
+        )
+          : <h3>We connect highly-skilled, remote professionals with companies working for the greater good.</h3>
         }
       </div>
-      <button className='create-profile-btn'>create your profile</button>
-      <h3 className='find-talent'><a href='#'>Find talent</a></h3>
+      <button className='create-profile-btn'><Link to="/signup">Create your Profile</Link></button>
+      <h3 className='find-talent'><Link to="/findTalent">Find Talent</Link></h3>
     </div>
   </div>
 );
