4b1ce4b6ed8386c020da1c4bc0cfcc68dbf21332
ryanferrer <ryferr8122@gmail.com>
2020-06-05 14:12:07 -0500

    added upper half

---
+++ src/components/parts/Jumbotron.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Jumbotron = () => (
+  <div className="jumbotron">
+    I am a jumbotron!
+  </div>
+);
+
+export default Jumbotron;
--- src/pages/FindTalent.jsx
+++ src/pages/FindTalent.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 
 const FindTalent = () => (
-  <div>
-    Behold! I am the Find Talent Page!
+  <div className="find-talent-page">
+    <div className="find-talent-upper">
+      <div className="find-talent-upper-left">
+        <h1>Make a greater impact with remote talent.</h1>
+
+        <p>
+          Our network of professionals are looking for meaningful assignment. Wel will review thousands of
+          highly qualified remote candidates to find the best match for your team.
+        </p>
+
+        <button className="button-1">Submit a Job Req</button>
+      </div>
+
+      <div className="find-talent-upper-graphics">
+
+      </div>
+    </div>
+
   </div>
 );
 
