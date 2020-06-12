4c5e0e0e9067658be688a0176155aec87bbeb465
ryanferrer <ryferr8122@gmail.com>
2020-06-12 15:30:22 -0500

    removed sub compoennt to signup

--- src/components/Auth/Registration.jsx
+++ src/components/Auth/Registration.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import NameEmailNumberPass from './regSubComponents/NameEmailNumberPass.jsx';
 
 const backend = 'http://3.21.186.204:3030';
 
