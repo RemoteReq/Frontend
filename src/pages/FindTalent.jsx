ee6880a7885fc70243341b6df00ab1997ec8d02d
ryanferrer <ryferr8122@gmail.com>
2020-06-23 21:07:57 -0500

    added required context to find talent

--- src/pages/FindTalent.jsx
+++ src/pages/FindTalent.jsx
@@ -10,8 +10,9 @@ const FindTalent = () => (
         <h1>Make a greater impact with remote talent.</h1>
 
         <p>
-          Our network of professionals are looking for meaningful assignment. Wel will review thousands of
-          highly qualified remote candidates to find the best match for your team.
+          Our network of remote professional are ready to help you do more good. Let us do the work for you.
+          We will find, vet, and connect you with the remote talent you need. 
+          Submit a job req to get started. Pay only for the candidates you choose to engage.
         </p>
 
         <button className="button-1">Submit a Job Req</button>
@@ -27,27 +28,27 @@ const FindTalent = () => (
         <img className="triplet-icon" src={triplet1}></img>
 
         <h3>Submit a Job Req</h3>
-        <p>Share your scope of work and profile of your ideal here.</p>
+        <p>Share your scope of work and the profile of your ideal hire.</p>
       </div>
 
       <div className="mid-triplet">
           <img className="triplet-icon" src={triplet2}></img>
 
           <h3>Get Matched</h3>
-          <p>Our talent matching algorithm generates your list of finalists.</p>
+          <p>Our recruiting algorithm will review thousands of remote professioanls to find your "best fit" matches.</p>
       </div>
 
       <div className="mid-triplet">
         <img className="triplet-icon" src={triplet3}></img>
 
           <h3>Interview and hire</h3>
-          <p>Interview, hire, and grow your team. Can this get any longer?</p>
+          <p>Review your "best fit" matches to find your remote unicorn.</p>
       </div>
     </div>
 
     <div className="find-talent-pricing">
       <h1>Simple, transparent pricing</h1>
-      <p>Explain the pricing here</p>
+      <p></p>
 
       <div className="card-container">
         <div className="pricing-card">
@@ -58,8 +59,8 @@ const FindTalent = () => (
 
           <div className="pricing-card-description">
             <p>For projects lasting 11 months or less</p>
-            <p>3 Finalist candidates</p>
-            <p>Temporary skills &amp; expertise</p>
+            <p>3 "best fit" candidates</p>
+            <p>Added capcity, skills, and expertise</p>
             <p>Negotioable hourly rates</p>
           </div>
 
@@ -75,7 +76,8 @@ const FindTalent = () => (
           <div className="pricing-card-description">
             <p>Grow your team with a remote hire</p>
             <p>3-5 Finalist Candidates</p>
-            <p>Added capacity, skills, &amp; expertise</p>
+            <p>Added capacity, skills, and expertise</p>
+            <p>Negotioable pay</p>
           </div>
 
           <button className="button-1">Submit a Job Req</button>
