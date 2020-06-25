6f35dfbd9ffd16b873a0ab9da5782d9dab1c4be5
ryanferrer <ryferr8122@gmail.com>
2020-06-25 02:53:59 -0500

    import landing page graphic

--- src/pages/LandingPage/LPHeader.jsx
+++ src/pages/LandingPage/LPHeader.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import LandingPageGraphic from '#assets/images/svgs/landing-page-graphic.svg';
 
 const LPHeader = () => (
   <div className='landingPage-header'>
-    {/* <div className="landingPage-overlay"></div> */}
+
     <div className='landingPage-heading-btns'>
       <div className='heading'>
         <h1>Find remote work with greater purpose.</h1>
-        { window.innerWidth > 767 ? (
+        { window.innerWidth > 767 ?
           <p>We connect highly-skilled, remote professionals with nonprofit and for-profit companies working for the greater good. Find a part-time gig, or full-time, today.</p>
-
-        )
-          : <p>We connect highly-skilled, remote professionals with companies working for the greater good.</p>
+          : 
+          <p>We connect highly-skilled, remote professionals with companies working for the greater good.</p>
         }
       </div>
 
@@ -29,8 +29,10 @@ const LPHeader = () => (
           </Link>
         </div>
       </div>
-
     </div>
+
+    {/* <LandingPageGraphic height={2000} width={2000}/> */}
+
   </div>
 );
 
