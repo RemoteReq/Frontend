e3d62d462ba23cb7deb7a7893f1741fd6835d56d
ryanferrer <ryferr8122@gmail.com>
2020-06-25 04:01:44 -0500

    removed section components and render graphics

--- src/pages/LandingPage/AboutMain.jsx
+++ src/pages/LandingPage/AboutMain.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Section from '../../components/parts/Section.jsx';
-import MiddleSection from '../../components/parts/MiddleSection.jsx';
-import ReverseSection from '../../components/parts/ReverseSection.jsx';
+import quickHiring from '#assets/images/pngs/quick-hiring.png'
+import Matches from '#assets/images/svgs/matches.svg';
 
 const AboutMain = () => (
   <div className='about-main'>
     <h3 className='eyebrow'>What makes us different?</h3>
 
-    <Section 
-      header={'Meet your personal recruiter'}
-      p={"No more lost hours scouring jobs boards. No more lengthy applications. Simply create a profile, and let us do the work for you."}
-      link={"Get started"}
-      to={"/signup"}
-    />
+    <section>
+      <div className="section-desc">
+        <h2>Meet your personal recruiter</h2>
+        <p>No more lost hours scouring jobs boards. No more lengthy applications. Simply create a profile, and let us do the work for you.</p>
+        <Link to={'/signup'} className="large-link">Get Started</Link>
+      </div>
 
-    <MiddleSection
-      header={"Remote + Purposeful"}
-      p={"No more watching from the sidelines. Do impactful work from anywhere."}
-    />
+      <div className="section-graphic">
+        <Matches className="svg-matches" height={'100%'} width={'100%'}/>
+      </div>
+    </section>
 
-    <div className='about remote-purposeful'>
-      {/* <h1>Remote + Purposeful</h1>
-      <p>No more watching from the sidelines. Do impactful work from anywhere.</p> */}
+
+    <section className="middle-section">
+      <h2>Remote + Purposeful</h2>
+      <p>No more watching from the sidelines. Do impactful work from anywhere.</p>
+    </section>
+
+    <div>
       <ul>
         <li>Health &amp; Wellness</li>
         <li>LGBTQIA+ Rights</li>
@@ -35,13 +38,17 @@ const AboutMain = () => (
       </ul>
     </div>
 
-    <ReverseSection
-      header={"Quick hiring process"}
-      p={"Everyone wins with quicker response times and faster hiring processes."}
-      link={"Find a job"}
-      to={"/signup"}
-    />
+    <section className="reverse-section">
+      <div className="section-desc">
+        <h2>Quick hiring process</h2>
+        <p>Everyone wins with quicker response times and faster hiring processes.</p>
+        <Link to="/signup" className="large-link">Find your job</Link>
+      </div>
 
+      <div className="section-graphic">
+        <img src={quickHiring}/>
+      </div>
+    </section>
   </div>
 );
 
