10b77d49575111467fd31b299e62091e3ce286dc
ryanferrer <ryferr8122@gmail.com>
2020-07-30 02:38:07 -0500

    rework vision complete

--- src/components/parts/Footer.jsx
+++ src/components/parts/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import RRLogo from '#assets/images/pngs/RR-cobalt.png';
+import Divider from './Divider.jsx';
 // import FaceBookIcon from '#assets/img/facebook.png';
 // import LinkedinIcon from '#assets/img/linkedin.png';
 // import TwitterIcon from '#assets/img/twitter.png';
@@ -8,13 +9,43 @@ import RRLogo from '#assets/images/pngs/RR-cobalt.png';
 const Footer = () => {
   return (
   <div className='footer'>
-    <ul className='footerLinks'>
-      <li><Link to="/about" className="small-link">About Us</Link></li>
-      <li><Link to="/signup" className="small-link">Find Jobs</Link></li>
-      <li><Link to="/findTalent" className="small-link">Find Talent</Link></li>
-      <li><Link to="/privacyPolicy" className="small-link">Our Privacy Policy</Link></li>
-      <li><Link to="termsOfUse" className="small-link">Terms of Use</Link></li>
-    </ul>
+    <div className='upper-footer'>
+      <ul className='footer-links'>
+        <li><Link to="/about" className="small-link">About Us</Link></li>
+        <li><Link to="/signup" className="small-link">Find Jobs</Link></li>
+        <li><Link to="/findTalent" className="small-link">Find Talent</Link></li>
+      </ul>
+
+      <div className="medias">
+        <ul className='social-media-list'>
+          <li>Facebook</li>
+          <li>Instagram</li>
+          <li>LinkedIn</li>
+        </ul>
+
+          <input/>
+      </div>
+    </div>
+
+    <Divider />
+
+    <div className='lower-footer'>
+      <span className="the-r">
+        <Link to="/">
+          <img src={ RRLogo } alt='remotereq logo'/>
+        </Link>
+
+        <p>
+          Slogan || copyright
+        </p>
+      </span>
+
+
+      <ul>
+        <li><Link to="/privacyPolicy" className="small-link">Our Privacy Policy</Link></li>
+        <li><Link to="termsOfUse" className="small-link">Terms of Use</Link></li>
+      </ul>
+    </div>
   </div>
   );
 };
