1deda8790047be233dcf3270d9f7f354e7a84dd2
ryanferrer <ryferr8122@gmail.com>
2020-10-01 04:43:41 -0500

    added candidate count to job listings

--- src/pages/Employer/EDashboard/jobList/JobListing.jsx
+++ src/pages/Employer/EDashboard/jobList/JobListing.jsx
@@ -3,9 +3,10 @@ import { Link } from 'react-router-dom';
 
 const JobListing = ({ job }) => {
   console.log(job);
+  const previewDetails = job.jobDetails.substring(0, 128).concat('...');
 
   return (
-  <div className="job-listing">
+  <div className="employer-job-listing">
 
     <Link
       to={{
@@ -18,6 +19,18 @@ const JobListing = ({ job }) => {
       <div>
         <div className="upper-job-listing">
           <h4 className="job-listing-title">{job.title}</h4>
+
+          {
+            job.matchesCandidateCount
+
+              ? <div className="match-counter">
+              <p>
+                {1}
+              </p>
+            </div>
+
+              : <div></div>
+          }
         </div>
 
         <h5>{job.companyName}</h5>
@@ -26,7 +39,7 @@ const JobListing = ({ job }) => {
 
         <div className="lower-job-listing">
           <p className="small-paragraph">
-            {job.jobDetails}
+            {previewDetails}
           </p>
         </div>
     </Link>
