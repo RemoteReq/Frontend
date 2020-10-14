014a1bec1fbf0e16b082f49ade44dd5d5d5cd4e8
ryanferrer <ryferr8122@gmail.com>
2020-10-14 16:51:15 -0500

    awaiting jbos

--- src/pages/Dashboard/JobList/JobList.jsx
+++ src/pages/Dashboard/JobList/JobList.jsx
@@ -4,9 +4,28 @@ import JobListing from './JobListing.jsx';
 // From here, loop over the inherited data from App.jsx and render and JobListings
 
 const JobList = ({ jobs }) => {
+  console.log('jobs in jobsList', jobs);
+
+  if (jobs) {
+    if (jobs.length === 0) {
+      return (
+        <AwaitingJobs />
+      );
+    }
+    return (
+        <MappedJobs jobs={jobs}/>
+    );
+  }
   return (
-    jobs
-      ? <div className='jobList'>
+    <div>
+      Retrieving jobs . . .
+    </div>
+  );
+};
+
+const MappedJobs = ({ jobs }) => {
+  return (
+    <div className='jobList'>
 
     <h3>Job Matches</h3>
 
@@ -15,16 +34,35 @@ const JobList = ({ jobs }) => {
         jobs.map((job, i) => {
           return (
             (
-            <JobListing key={i} job={job}/>
+              <JobListing key={i} job={job}/>
             )
           );
         })
       }
       </div>
     </div>
+  );
+};
 
-      : <div>
-      Retreiving Jobs . . .
+const AwaitingJobs = () => {
+  return (
+    <div className="jobList">
+      <h3>Job Matches</h3>
+
+      <div className="await-jobs">
+
+        <div className="await-jobs-icon">
+          <img />
+        </div>
+
+        <h4>
+          We're working hard to match you with the best jobs!
+        </h4>
+
+        <p className="small-paragraph">
+          Expect an email with your first matches, and then check back here for a list of jobs.
+        </p>
+      </div>
     </div>
   );
 };
