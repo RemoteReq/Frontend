e8e44c05b6934a7b7e3c68c1658eb19736ecd29a
ryanferrer <ryferr8122@gmail.com>
2020-10-28 16:04:01 -0500

    hamburger menu icon fix

--- src/components/parts/Navigation2.jsx
+++ src/components/parts/Navigation2.jsx
@@ -80,28 +80,4 @@ class Navigation2 extends Component {
   }
 }
 
-// const Navigation3 = (props) => {
-//   const history = useHistory();
-
-//   return (
-//     <nav className='dashboard-navBar'>
-//       <Link className='dashboard-home stealth-link' to="/dashboard">
-//         <img src={ RemoteReq } className='remotereq-name' alt='remote' />
-//         <p>Job Board</p>
-//       </Link>
-
-//       <div className='dashboard-links'>
-
-//         <a className="large-link"
-//           onClick={() => {
-//             return auth.logout(() => {
-//               history.push('/signin');
-//             });
-//           }}
-//         >Sign out</a>
-//       </div>
-//     </nav>
-//   );
-// };
-
 export default withRouter(Navigation2);
