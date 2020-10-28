2d282748b00c152e43c5af4e8239982d8a17f3b0
ryanferrer <ryferr8122@gmail.com>
2020-10-28 15:31:36 -0500

    job seeker dashboard is responsive

--- src/components/parts/Navigation2.jsx
+++ src/components/parts/Navigation2.jsx
@@ -1,30 +1,105 @@
-import React from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 import RemoteReq from '#assets/images/pngs/RR-cobalt.png';
 import auth from '../Auth/Auth.jsx';
 
-const Navigation2 = (props) => {
-  const history = useHistory();
+class Navigation2 extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <nav className='dashboard-navBar'>
-      <Link className='dashboard-home stealth-link' to="/dashboard">
-        <img src={ RemoteReq } className='remotereq-name' alt='remote' />
-        <p>Job Board</p>
-      </Link>
+    this.state = {
+      showHamburgerMenu: false,
+    };
 
-      <div className='dashboard-links'>
+    this.toggleHamburgerMenu = this.toggleHamburgerMenu.bind(this);
+    this.signOut = this.signOut.bind(this);
+  }
 
-        <a className="large-link"
-          onClick={() => {
-            return auth.logout(() => {
-              history.push('/signin');
-            });
-          }}
-        >Sign out</a>
-      </div>
+  signOut(e) {
+    e.preventDefault();
+
+    auth.logout(() => {
+      this.props.history.push('/signin');
+    });
+  }
+
+  toggleHamburgerMenu(e) {
+    e.preventDefault();
+
+    const { showHamburgerMenu } = this.state;
+
+    if (showHamburgerMenu) {
+      this.setState({
+        showHamburgerMenu: false,
+      });
+    } else {
+      this.setState({
+        showHamburgerMenu: true,
+      });
+    }
+  }
+
+  render() {
+    const { showHamburgerMenu } = this.state;
+
+    return (
+      <nav className='dashboard-navBar'>
+        <Link to="/dashboard" className="dashboard-home">
+          <img src={ RemoteReq } className='remotereq-name' alt=""/>
+          <p>Job Board</p>
+        </Link>
+
+        <div className="dashboard-links">
+          <a className="large-link"
+            onClick={(e) => { this.signOut(e); }}
+          >Sign out</a>
+        </div>
+
+        <div className={`hamburger-menu ${showHamburgerMenu ? 'show' : 'hide'}`}>
+          <Link to="/settings/profile">
+            <button className='button-2'>Settings</button>
+          </Link>
+
+          <a className="large-link"
+            onClick={(e) => { this.signOut(e); }}
+          >Sign out</a>
+        </div>
+
+        <div
+          className="hamburger-menu-icon"
+          onClick={(e) => { this.toggleHamburgerMenu(e); }}
+        >
+          <p className={`${showHamburgerMenu ? 'hide' : 'show'}`}>=</p>
+          <p className={`${showHamburgerMenu ? 'show' : 'hide'}`}>x</p>
+        </div>
+
     </nav>
-  );
-};
+    );
+  }
+}
 
-export default Navigation2;
+// const Navigation3 = (props) => {
+//   const history = useHistory();
+
+//   return (
+//     <nav className='dashboard-navBar'>
+//       <Link className='dashboard-home stealth-link' to="/dashboard">
+//         <img src={ RemoteReq } className='remotereq-name' alt='remote' />
+//         <p>Job Board</p>
+//       </Link>
+
+//       <div className='dashboard-links'>
+
+//         <a className="large-link"
+//           onClick={() => {
+//             return auth.logout(() => {
+//               history.push('/signin');
+//             });
+//           }}
+//         >Sign out</a>
+//       </div>
+//     </nav>
+//   );
+// };
+
+export default withRouter(Navigation2);
