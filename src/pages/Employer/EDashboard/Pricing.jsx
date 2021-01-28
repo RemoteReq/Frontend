a81bb7dadf22ebff4ecede00edb1380b20ac8e41
ryanferrer <ryferr8122@gmail.com>
2021-01-28 15:58:52 -0600

    language change

--- src/pages/Employer/EDashboard/Pricing.jsx
+++ src/pages/Employer/EDashboard/Pricing.jsx
@@ -5,7 +5,7 @@ const Pricing = () => {
   return (
     <div className="message">
       <p className="small-paragraph">
-        Pay just $100 for detailed access to your candidate matches. <HashLink to="/find-talent#pricing">Read more</HashLink>
+        Pay only $100 for detailed access to your candidate matches. <HashLink to="/find-talent#pricing">Read more</HashLink>
       </p>
     </div>
   );
