5e9c0ee956c08fc1e6608327d4ede93405c08ec7
ryanferrer <ryferr8122@gmail.com>
2021-02-04 14:45:25 -0600

    updated FAQ

--- assets/faq/faq.js
+++ assets/faq/faq.js
@@ -8,43 +8,55 @@ const employerFaq = [
     answer: 'Nothing. Employer profiles are free to create and post remote jobs. Only pay $100 to view your candidate matches for each of your remote roles. Then, if you hire a candidate we connect you to, pay a $900 placement fee for part-time roles or $2,400 for full-time roles.',
   },
   {
-    question: 'Who can post on RemoteReq?',
-    answer: 'Nonprofit organizations or mission-focused, for-profit organizations of all sizes. Is the primary focus of your business to address an issue facing your community, country, or world? If so, then RemoteReq would love to help you find the talent you need to further your cause.',
+    question: 'How much does it cost to post a job on RemoteReq.com?',
+    answer: "Nothing. Employer profiles are free to create, and posting remote jobs is also free. \n\n Our recruiting algorithm will immediately return your 'best fit' candidate matches (up to 5 candidates). Next, pay only $100 for detailed access to your 'best fit' candidate matches, and start interviewing immediately",
   },
   {
     question: 'Why are their two steps to the checkout process for employers?',
-    answer: 'We are saving you money. On other platforms, the cost to post (usually a few thousand dollars) is paid all upfront, regardless of output. For cost-sensitive, nonprofits and small businesses, even a few hundred bucks can make a big difference. It can be the difference between posting to a platform and reaching a potential pool of candidates or not.',
+    answer: " We want your dollars focused on your mission. Our cost-saving, two-step checkout process ensures that employers only pay for who they hire. After we find your best fit 'candidate matches,' here's what to expect - \n\n Step One: Pay $100 for instant access to detailed profiles for your candidate matches \n\n Step Two:  When you hire one of your candidate matches, pay $900 per part-time hire and $2400 per full-time hire \n\n Other job search platforms and jobs boards charge exorbitant upfront costs with no guarantee of hire. That works for big corporations with deep pockets, but not the little guys. Return on investment is mission-critical.",
   },
   {
-    question: 'How much does it cost to make a hire on RemoteReq.com?',
-    answer: "On RemoteReq, you pay once to access your candidate matches ($100), then you pay only for the candidates you hire. ($900 per hire for part-time roles, and $2,400 per hire for full-time roles.) \n\n That's why you on RemoteReq, you pay once to access your candidate matches ($100), then you pay only for the candidates you hire. \n\n ($900 per hire for part-time roles, and $2,400 per hire for full-time roles.) \n\n If you do the cost-per-hire analysis, you will see that our rates are more competitive than the competition. If you do the cost-per-hire analysis, you will see that our rates are more competitive than the competition.",
+    question: 'How much does it cost to hire on RemoteReq.com?',
+    answer: 'Post unlimited jobs for free on RemoteReq! After we match you to your "best fit" candidate matches pay only $100 for instant access to their detailed profiles, resumes, and more. \n\n After interviewing your candidate matches, pay only $900 per hire for part-time roles, and $2,400 per hire for full-time roles.',
+  },
+  {
+    question: 'Who should post their jobs on RemoteReq?',
+    answer: 'Social impact organizations (nonprofits and mission-focused, for-profits) with roles that can be done from home or remotely. \n\n If your business is addressing an issue facing your community, country, or world, then RemoteReq would love to help you find the talent you need to increase your impact.',
+  },
+  {
+    question: 'Why RemoteReq versus some other jobs posting site?',
+    answer: 'Glad you asked. It starts with who we are as a team - our Founder is a veteran, Talent Consultant and Executive Recruiter who has spent his career helping social impact organizations hire better. Having placed high-profile CEOs and Executive Directors across the country, he also enjoys helping nonprofits and mission-focused for-profits to solve their every day talent challenges. RemoteReq is the solution. \n\n Our employer partners receive: \n\n - A faster and smarter candidate outreach process (We have the expertise, national network, and machine learning you need to find the best available talent.) \n\n - More diverse candidate pools (Our reach is far and wide, plus we have relationships with groups, institutions, and associations from all backgrounds.) \n\n - Less lift on your team (Rather than wasting man-hours sifting through unqualified candidates who care nothing about your mission - let us be your  go-to talent partner.) \n\n - Savings when compared to our competition (We want your dollars going to your mission. Our cost-saving, two-step checkout process ensures that you pay for only who you hire.)',
+  },
+  {
+    question: 'How long will it take for me to be matched to my "best fit" candidates?',
+    answer: 'Usually, it is instant. Occasionally, it will take us a bit longer. A few days, no more than a week. New job seeker profiles are being added daily, so our network is constantly growing with skilled job seekers looking to make a remote impact.',
   },
 ];
 
 const jobSeekerFaq = [
   {
     question: 'How much does it cost to make a job seeker profile on RemoteReq.com?',
-    answer: 'Nothing. Job seeker profiles are free to create. It will cost you about 8 minutes of your time.',
+    answer: 'Nothing. Job seeker profiles are free to create. It will take you about 8 minutes to complete your profile and questionnaire.',
   },
   {
     question: "I just created a job seeker profile on RemoteReq. How long before I'll get a job match? Should I continue looking for jobs?",
-    answer: "It all depends. We're aggressively pursuing employers to have more and more jobs added to the platform. Each week, we’re seeing new jobs added. The more jobs added the more opportunities for matches. As soon as you have a potential match we will be in touch to invite you to engage with a your next potential employer.",
+    answer: 'Yes, keep your search alive. We\'re aggressively pursuing employers to add more jobs added to the platform, and each week, we’re seeing new jobs added. As soon as you have been matched to one of our jobs we will be in touch to  connect you with the hiring manager.',
   },
   {
     question: 'Should I actually fill out these short-answer questions?',
     answer: 'Yes! The more employers know about you, the more they have to work with. So, go the extra mile. Post thoughtful responses and upload an updated resume. It could be the difference between a call-back, or not.',
   },
   {
-    question: 'How can I help mission-focused organizations in my community?',
-    answer: 'Encourage them to visit RemoteReq.com, to create a free, employer account and to post a remote job their hiring for. (The registration process is easier than setting up a Facebook profile.) After seeing how quickly their matched to great candidates, and how affordable our rates are, they will see in what we have to offer.',
+    question: 'How will I be notified of a potential match?',
+    answer: 'We will email you. Also, your matches will appear in your dashboard. Again, keep your job search alive, see us your personal search partner, and keep your head up. Job searching can be a difficult and frustrating process, at times. Together, we will get there.',
   },
 ];
 
 const generalFaq = [
-  // {
-  //   question: 'Why RemoteReq is a better option than other job search platforms out there?',
-  //   answer: '',
-  // },
+  {
+    question: 'How can I help mission-focused organizations in my community?',
+    answer: 'Encourage them to visit RemoteReq.com, to create a free, employer account and to post a remote job their hiring for. (The registration process is easier than setting up a Facebook profile.) After seeing how quickly their matched to great candidates, and how affordable our rates are, they will see in what we have to offer.',
+  },
   {
     question: 'Why mission-focused remote jobs? Why not just remote jobs?',
     answer: "We (not-so) secretly want to make a big impact. RemoteReq allows us to build a bench of highly-qualified remote professionals who are passionate about making their community, country, or world a better place. That is something we can get behind. Our team is a bunch of digital-nomads, and passionate change-agents. We value flexibility in our work, but we also value purpose. \n\n Finally, there are plenty of options for the 'big guys' to find the talent they need to drive huge profits. Nothing wrong with that. We just want to focus our efforts and network on helping the 'good guys' to make a great impact. From anywhere.",
