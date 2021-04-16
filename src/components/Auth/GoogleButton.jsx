ac599ef89b0564847176480d00614b052cf8519c
ryanferrer <ryferr8122@gmail.com>
2021-04-16 15:58:12 -0500

    method onsuccess and onfailure

--- src/components/Auth/GoogleButton.jsx
+++ src/components/Auth/GoogleButton.jsx
@@ -6,35 +6,54 @@ import GoogleLogo from '#assets/images/pngs/google-btn.png';
 // can only use Production URL in localhost and production due to app setup in Google services
 const backend = process.env.BASE_URL;
 
-const onSuccess = (response) => {
-  console.log('google sign in success!', response);
+// const onSuccess = (response) => {
+//   console.log('google sign in success!', response);
 
-  const profile = response.profileObj;
+//   const profile = response.profileObj;
 
-  return (
-    <Redirect
-      to={{
-        pathname: '/dashboard',
-        state: {
-          profile,
-        },
-      }}
-    />
-  );
-};
+//   return (
+//     <Redirect
+//       to={{
+//         pathname: '/dashboard',
+//         state: {
+//           profile,
+//         },
+//       }}
+//     />
+//   );
+// };
 
-const onFailure = (error) => {
-  console.log('google sign in error!', error);
-};
+// const onFailure = (error) => {
+//   console.log('google sign in error!', error);
+// };
 
 class GoogleButton extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoggedIn: false,
+    };
+
+    this.onSuccess = this.onSuccess.bind(this);
+    this.onFailure = this.onFailure.bind(this);
+  }
+
+  onSuccess(response) {
+    console.log('method logging in works!', response);
+  }
+
+  onFailure(error) {
+    console.log('method failure works at least', error);
+  }
+
   render() {
     return (
       <GoogleLogin
         clientId="106530052018-epup7ot9lju37ugc54kjerd79av1pat0.apps.googleusercontent.com"
         buttonText="Sign in with Google"
-        onSuccess={onSuccess}
-        onFailure={onFailure}
+        onSuccess={this.onSuccess}
+        onFailure={this.onFailure}
         isSignedIn={true}
         redirectUri={'/dashboard'}
         className="google-button"
